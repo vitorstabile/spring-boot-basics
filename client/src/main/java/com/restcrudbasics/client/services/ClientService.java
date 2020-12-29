f3702318c0758afad630c56027fb03bed6f06b77
Vitor Garcia <vitor.stabile@hotmail.com>
2020-12-29 16:41:42 -0300

    Class14 - seeding database

--- client/src/main/java/com/restcrudbasics/client/services/ClientService.java
+++ client/src/main/java/com/restcrudbasics/client/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.restcrudbasics.client.entities.Client;
 import com.restcrudbasics.client.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired //This annotation will make this instance auto injected (Remember the @Repository annotation in ClientRepository class)
 	private ClientRepository repository;
 	
+	@Transactional(readOnly = true) // Transactional close when make the REST request. Good Practice in Program
 	public List<Client> findAll(){
 		return repository.findAll(); // When we make repository. we have a tons of methods(FindAll, delete, insert, save)
 	}
