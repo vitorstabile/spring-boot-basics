cb2cc98310e0d60e29d755a9c26bde80cadc96a0
Vitor Garcia <vitor.stabile@hotmail.com>
2020-12-29 14:12:21 -0300

    Class10 - Implement Service

--- client/src/main/java/com/restcrudbasics/client/resources/ClientResource.java
+++ client/src/main/java/com/restcrudbasics/client/resources/ClientResource.java
@@ -1,26 +1,26 @@
 package com.restcrudbasics.client.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restcrudbasics.client.entities.Client;
+import com.restcrudbasics.client.services.ClientService;
 
 @RestController
 @RequestMapping(value ="/clients") //the REST route when you make the requisition. Use the plural form of the entity
 public class ClientResource {
 	
+	@Autowired //This annotation will make this instance auto injected (Remember the @Service annotation in ClientService class)
+	private ClientService service;
+	
 	@GetMapping //endpoint GET
 	public ResponseEntity<List<Client>> findAll(){ //This is a endpoint to find a client by id
-		List<Client> list = new ArrayList<>(); //to test the request, we gonna make a list
-		list.add(new Client(1L, "João", "222.222.222-22", 2000.0, Instant.parse("2017-02-03T11:35:30.00Z"), 2)); 
-		list.add(new Client(2L, "Maria", "333.333.333-33", 3000.0, Instant.parse("2017-02-03T11:35:30.00Z"), 3));
-		list.add(new Client(3L, "Jose", "444.444.444-44", 4000.0, Instant.parse("2017-02-03T11:35:30.00Z"), 4));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
---
+++ client/src/main/java/com/restcrudbasics/client/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.restcrudbasics.client.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.restcrudbasics.client.entities.Client;
+import com.restcrudbasics.client.repositories.ClientRepository;
+
+@Service // this annotation will register the class ClientService as a dependency injection component and will be management by spring
+public class ClientService {
+	
+	@Autowired //This annotation will make this instance auto injected (Remember the @Repository annotation in ClientRepository class)
+	private ClientRepository repository;
+	
+	public List<Client> findAll(){
+		return repository.findAll(); // When we make repository. we have a tons of methods(FindAll, delete, insert, save)
+	}
+
+}
