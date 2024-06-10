7696e0635c32133d4aaa782c25fed8e77c1728c8
vitorstabile <vitor.stabile@hotmail.com>
2024-06-10 12:26:05 +0100

    Add in Memory Credentials

--- learn-spring-security/src/main/java/com/vitorproject/springboot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/vitorproject/springboot/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -38,5 +41,21 @@ public class BasicAuthSecurityConfiguration {
 
             }
         };
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+
+        var user = User.withUsername("userexample")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
